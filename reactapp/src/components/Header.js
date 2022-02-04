3a2afd70df57d059afee5528e09f834ba7dd3cd8
Matteo Casonato <matteocasonato@protonmail.com>
2022-02-04 11:02:24 +0100

    header

--- reactapp/src/components/Header.js
+++ reactapp/src/components/Header.js
@@ -6,6 +6,7 @@ export function Header({currentAddress, balance, seller}) {
     <header>
         <div className='main-app'>
             {/* <img id="logo" alt="Logo Team Oberon" src={logo}></img> */}
+            <h1>ShopChain</h1>
             <p>Current Address: {currentAddress} <br/>
             Current balance: {balance} <br/>
             Seller Address: {seller}</p>
